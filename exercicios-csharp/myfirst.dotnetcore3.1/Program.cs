9c639f92bac65392957480dca257a722cbfa28e7
Italo Bueno <91482316+Bueno-Italo@users.noreply.github.com>
2023-10-01 08:47:04 -0300

    Criei uma aplicação que processa a idade de um usuário.

---
+++ exercicios-csharp/myfirst.dotnetcore3.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace myfirst.dotnetcore3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessarIdade();
+            Console.ReadKey();
+        }
+
+        public static void ProcessarIdade() 
+        {
+            int idade;
+
+            Console.Write("informe uma idade: ");
+            int.TryParse(Console.ReadLine(), out idade);
+
+            if (idade < 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sem Permissão");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Permissão Concedida");
+            }
+        }
+
+    }
+}
