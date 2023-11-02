c779c83a31aa01c851a574598f9e2a7250406226
Italo Bueno <91482316+Bueno-Italo@users.noreply.github.com>
2023-11-02 00:50:56 -0300

    Atualização em Calcular Idade

--- exercicios-csharp/myfirst.dotnetcore3.1/Program.cs
+++ exercicios-csharp/myfirst.dotnetcore3.1/Program.cs
@@ -9,7 +9,7 @@ namespace myfirst.dotnetcore3._1
     {
         static void Main(string[] args)
         {
-            ExibirIdade();
+            CalculoIdadePessoa();
             Console.ReadKey();
         }
 
@@ -37,22 +37,22 @@ namespace myfirst.dotnetcore3._1
             decimal salarioAtual;
             Console.WriteLine("informe seu salário: ");
             decimal.TryParse(Console.ReadLine(), out salarioAtual);
-            
+
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"Seu salario atual é: {salarioAtual}");
-            
+
             Console.ResetColor();
             Console.WriteLine("Estamos calculando seu salário... ");
-            
+
             var novoSalario = CalcularSalario(salarioAtual);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"Seu novo salario atual é : {novoSalario}");
         }
 
-        public static decimal CalcularSalario(decimal salarioAtual) 
+        public static decimal CalcularSalario(decimal salarioAtual)
         {
             decimal novoSalario;
-            if(salarioAtual < 1700) 
+            if (salarioAtual < 1700)
             {
                 novoSalario = salarioAtual + 300;
             }
@@ -70,41 +70,50 @@ namespace myfirst.dotnetcore3._1
             string nomePessoa;
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Informe seu Nome: ");
-            nomePessoa=Console.ReadLine();
+            nomePessoa = Console.ReadLine();
 
             string sobreNome;
             Console.WriteLine("Informe seu sobrenome: ");
-            sobreNome=Console.ReadLine();
+            sobreNome = Console.ReadLine();
 
             string textoConcatenado = nomePessoa + " " + sobreNome;
-            Console.Write($"nome e sobrenome é:  {textoConcatenado}");      
+            Console.Write($"nome e sobrenome é:  {textoConcatenado}");
         }
 
-        public static void  ExibirIdade()
+        public static void CalculoIdadePessoa()
+
+
 
         {
-            int Ano;
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Informe o ano de nascimento: ");
-            Ano = Convert.ToInt32(Console.ReadLine());
+            decimal dia;
+            Console.WriteLine("Informe o dia do seu nascimento: ");
+
+
+
+            int mes;
+            Console.WriteLine("Informe o mes do seu nascimento: ");
+            
+
+
+            int ano_nascimento = Int32.Parse(Console.ReadLine());
+            Console.Write("Informe o ano atual: ");
+            
+
+            int ano_atual = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o ano do seu nascimento: ");
+            ano_nascimento += Int32.Parse(Console.ReadLine());
+
+            int idade_anos = ano_atual - ano_nascimento;
+            Console.WriteLine("\nA idade em anos é: " + idade_anos);
+
+
             
             
-            string Nome;
-            Console.WriteLine(" Informe o seu nome: ");
-            Nome = Console.ReadLine();
 
-            string TextoConcatenado = Ano + " " + Nome;
-            Console.Write(TextoConcatenado);
-
-
-
-
-
-
-
-
-
+             
         }
+
+        
 
     }
 }
