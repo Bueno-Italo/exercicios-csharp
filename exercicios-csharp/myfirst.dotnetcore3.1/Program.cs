70b2f58d3193ac524426895ac0f3f3a79002759b
Italo Bueno <91482316+Bueno-Italo@users.noreply.github.com>
2023-12-07 16:33:15 -0300

    Exercicios saida de dados C#

--- exercicios-csharp/myfirst.dotnetcore3.1/Program.cs
+++ exercicios-csharp/myfirst.dotnetcore3.1/Program.cs
@@ -6,7 +6,7 @@ namespace myfirst.dotnetcore3._1
     {
         static void Main(string[] args)
         {
-            CalculoIdadePessoa();
+            Exercicios();
             Console.ReadKey();
         }
 
@@ -108,6 +108,23 @@ namespace myfirst.dotnetcore3._1
             return idade;
         }
 
+        public static  void Exercicios()
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de Escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos: " + produto1 + ", cujo o preço é: $ " + preco1);
+            Console.WriteLine(produto2 + ", cujo preco é: $" + preco2);
+
+            Console.WriteLine("Registro: " + idade + " anos de idade, " + "código " + codigo + " e genero: " + genero);
+        }
 
     }
 }
