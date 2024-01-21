4139c1dd26d846411747ad5c5af262828b431633
Italo Bueno <91482316+Bueno-Italo@users.noreply.github.com>
2024-01-20 21:47:44 -0300

    FunçõesAritmeticas

---
+++ exercicios-csharp/FunçõesAritmeticas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+Console.WriteLine(" ##\n Funções Aritméticas! ##");
+
+double x = 3.0;
+
+double y = 4.0;
+
+double z = -5.0;
+
+double A, B, C;
+
+A = Math.Pow(x, y);
+
+B = Math.Pow(x, 2.0);
+
+C = Math.Pow(5.0, 2.0);
+
+Console.WriteLine(x + " Elevado a " + y + " = " + A);
+
+Console.WriteLine(x + " Elevado ao quadrado = " + B);
+
+Console.WriteLine("5 elevado ao quadrado = " + C);
+
+A = Math.Abs(y);
+B = Math.Abs(z);
+
+Console.WriteLine("Valor absoluto de  " +y + " = " + A);
+Console.WriteLine("Valor absoluto de " +z + " =  + B ");
+Console.ReadLine();
